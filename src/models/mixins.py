b337796c4e5b3472e28dd1e820560a4a1fc27c68
ivan <i.razgonyaev@estesis.tech>
2024-03-22 16:00:42 +0700

    feature(mixins): Добавил миксины даты создания и обновления записей.

--- src/models/mixins.py
+++ src/models/mixins.py
@@ -6,15 +6,22 @@ from sqlalchemy.orm import Mapped, mapped_column
 from src.common.datetime import DateTimeManager
 
 
-class DateTimeMixin:
+class CreatedAtMixin:
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         default=DateTimeManager.get_datetime,
         comment="DateTime of create record",
     )
+
+
+class UpdatedAtMixin:
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         default=DateTimeManager.get_datetime,
         onupdate=DateTimeManager.get_datetime,
         comment="DateTime of update record",
     )
+
+
+class DateTimeMixin(CreatedAtMixin, UpdatedAtMixin):
+    pass
